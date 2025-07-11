489730a6ff577479170cb3ad64eaf49232981325
Claudiu Limban <claudiu.limban@qualitance.com>
2025-07-11 15:02:03 +0300

    Form validation

--- server/api/signup.post.ts
+++ server/api/signup.post.ts
@@ -1,15 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import supabase from './utils/supabase'
 
 export default eventHandler(async (event) => {
+  const { email, password, checkbox: opt_in } = await readBody(event)
 
-  const supabase = createClient(
-    process.env.SUPABASE_URL as string,
-    process.env.SUPABASE_KEY as string
-  )
-
-  const postBody = await readBody(event)
-
-  const { data, error } = await supabase.auth.signUp(postBody)
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: { // Metadata which goes into the user profile table
+      data: {
+        opt_in
+      }
+    }
+  })
 
   if (error)
     throw createError({
